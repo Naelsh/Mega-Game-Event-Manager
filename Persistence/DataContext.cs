289e1319c1c03f37e0977bf2a880d54288430996
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-19 06:26:38 +0200

    Add DataContext

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,42 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Activity> Activities { get; set; }
+
+        public DataContext() { }
+
+        public DataContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            #region ConnectionStrings
+            // Laptop
+            string connectionString =
+                @"Data Source=DESKTOP-EJ7V12L\SQLEXPRESS01;" +
+                @"Initial Catalog = MegaGameEventManager;" +
+                @"Integrated Security=true";
+
+            //Stationär
+            //string connectionString =
+            //    @"Data Source=DESKTOP-JC3MCVE;" +
+            //    @"Initial Catalog = MegaGameEventManager;" +
+            //    @"Integrated Security=true";
+            #endregion
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
