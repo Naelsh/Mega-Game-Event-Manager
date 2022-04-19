3a0b53f70bfba001932a28d55fd4318fb63899a4
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-19 05:53:10 +0200

    update strings to nullable

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -9,10 +9,10 @@ namespace Domain
     public class Activity
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public string Description { get; set; }
-        public string Location { get; set; }
+        public string? Description { get; set; }
+        public string? Location { get; set; }
     }
 }
