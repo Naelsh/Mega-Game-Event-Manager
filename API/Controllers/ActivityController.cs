7046c91cebcf0f9da9456e64cc378a2c29cc23d2
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-19 14:22:52 +0200

    Add activity controller

---
+++ API/Controllers/ActivityController.cs
@@ -0,0 +1,52 @@
+﻿using Application.Models.Activity;
+using Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class ActivityController : BaseController
+{
+    private readonly IActivityService _service;
+
+    public ActivityController(
+        IActivityService service
+        )
+    {
+        _service = service;
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var activity = _service.GetById(id);
+        return Ok(activity);
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var activities = _service.GetAll();
+        return Ok(activities);
+    }
+
+    [HttpPost]
+    public IActionResult Post(ActivityPostRequest model)
+    {
+        _service.Post(model);
+        return Ok(new { message = "Activity created successfully" });
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Put(int id, ActivityUpdateRequest model)
+    {
+        _service.Update(id, model);
+        return Ok(new { message = "Activity updated successfully" });
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        _service.Delete(id);
+        return Ok(new { message = "Activity deleted succesfully" });
+    }
+}
