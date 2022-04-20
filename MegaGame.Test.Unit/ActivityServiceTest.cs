ada1a73b0933c036ca139e4b6c15767843d08886
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-20 13:59:18 +0200

    some fiddeling

---
+++ MegaGame.Test.Unit/ActivityServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using API.Controllers;
+using Application.Helpers;
+using Application.Services;
+using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaGame.Test.Unit;
+
+[TestFixture]
+class ActivityServiceTest
+{
+    private ActivityController GetActivityController(string testName)
+    {
+        var context = TestAddon.GetTestContext(testName);
+        var mapper = new AutoMapperProfile();
+        var activityService = new ActivityService(context, mapper);
+        return new ActivityController();
+    }
+
+    [Test]
+    public void PostActivity_ValidInput_AddNewEntry()
+    {
+        int expected = 1;
+
+        using var context = TestAddon.GetContext(nameof(PostActivity_ValidInput_AddNewEntry));
+
+        Assert.AreEqual(expected, 1);
+    }
+}
---
+++ MegaGame.Test.Unit/TestAddon.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaGame.Test.Unit;
+
+public class TestAddon
+{
+    internal static DataContext GetTestContext(string testName)
+    {
+        var builder = new DbContextOptionsBuilder();
+        builder.UseInMemoryDatabase(testName);
+        return new DataContext(builder.Options);
+    }
+}
