d98ce3ee162724ddbc90123206290c895e17d454
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-21 14:51:57 +0200

    replaced nunit with xunit

---
+++ MegaGame.Test.Unit/ExampleTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Services;
+
+namespace MegaGame.Test.Unit;
+
+[TestFixture]
+internal class ExampleTest
+{
+    [Test]
+    public void Test()
+    {
+        var service = new ActivityService();
+        Assert.IsTrue(true);
+    }
+}
---
+++ MegagameEventManager.Test.Unit/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace MegagameEventManager.Test.Unit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
