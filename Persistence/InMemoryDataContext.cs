e5b7d08e853e485fa53a2c5eb788faf18232c967
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-21 15:11:50 +0200

    add datacontext for inmemory and sqlite

---
+++ Persistence/InMemoryDataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Persistence;
+
+public class InMemoryDataContext : DataContext
+{
+    private readonly string _databaseName;
+
+    public InMemoryDataContext(IConfiguration configuration) : base(configuration)
+    {
+        _databaseName = "test";
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // connect to sqlite database
+        options.UseInMemoryDatabase(_databaseName);
+    }
+}
---
+++ Persistence/SqliteDataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Persistence;
+
+public class SqliteDataContext : DataContext
+{
+    public SqliteDataContext(IConfiguration configuration) : base(configuration) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // connect to sqlite database
+        options.UseSqlite(Configuration.GetConnectionString("WebApiDatabase"));
+    }
+}
