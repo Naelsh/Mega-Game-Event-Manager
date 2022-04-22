509958641385d959087dfaa3db714e4d78b6d7ba
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-22 13:53:33 +0200

    add calendar package and implement in form

--- Frontend/mega_game_event_ui/src/Activities/ActivityForm.js
+++ Frontend/mega_game_event_ui/src/Activities/ActivityForm.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Input } from 'semantic-ui-react';
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
 
 const ActivityFormController = () => {
+  const [value, onChange] = useState(new Date());
+
   return (
     <Form>
-      <Form.Group widths="equal">
-        <Form.Field
-          id="Name"
-          control={Input}
-          label="Activity Name"
-          placeholder="Watch the skies"
-        />
-      </Form.Group>
+      <Form.Field
+        id="name"
+        control={Input}
+        label="Activity Name"
+        placeholder="Watch the skies"
+      />
+      <Form.Field
+        id="description"
+        control={Input}
+        label="Description"
+        placeholder="Once upon a time..."
+      />
+      <Calendar onChange={onChange} value={value}/>
     </Form>
   );
 }
