720658e7f579e108e2cdfa92bcdf0f9da0616630
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-22 21:38:48 +0200

    add post in frontend

--- Frontend/mega_game_event_ui/src/Activities/ActivityForm.js
+++ Frontend/mega_game_event_ui/src/Activities/ActivityForm.js
@@ -1,27 +1,62 @@
 import React, { useState } from "react";
-import { Form, Input } from 'semantic-ui-react';
-import Calendar from 'react-calendar'
+import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import '../Styles/Calendar.css';
 
-const ActivityFormController = () => {
-  const [value, onChange] = useState(new Date());
+function ActivityFormController () {
+  const [inputs, setInputs] = useState({});
 
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}));
+  }
+
+  const handleStartDateChange = (value) => {
+    setInputs(values => ({...values, ["startDate"]: value}));
+  }
+
+  const handleEndDateChange = (value) => {
+    setInputs(values => ({...values, ["endDate"]: value}));
+  }
+
+  const handleSubmit = (event) => {
+    fetch("https://localhost:7160/activity",
+    {
+      method: 'POST',
+      mode: 'cors',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(inputs)
+    });
+
+    event.preventDefault();
+    // alert(JSON.stringify(inputs))
+  }
+  
   return (
-    <Form>
-      <Form.Field
-        id="name"
-        control={Input}
-        label="Activity Name"
-        placeholder="Watch the skies"
+    <form onSubmit={handleSubmit}>
+      <label>Activity Name
+      <input
+        type="text"
+        name="name"
+        value={inputs.name || ""}
+        onChange={handleChange}
       />
-      <Form.Field
-        id="description"
-        control={Input}
-        label="Description"
-        placeholder="Once upon a time..."
-      />
-      <Calendar onChange={onChange} value={value}/>
-    </Form>
+      </label>
+      <label>Description
+        <input
+          type="text"
+          name="description"
+          value={inputs.description || ""}
+          onChange={handleChange}
+        />
+      </label>
+      <p>Start Date</p>
+      <Calendar value={inputs.startDate} onChange={handleStartDateChange}/>
+      <p>End Date</p>
+      <Calendar value={inputs.endDate} onChange={handleEndDateChange}/>
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 
