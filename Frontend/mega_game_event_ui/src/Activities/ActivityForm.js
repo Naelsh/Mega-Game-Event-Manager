dfc7390dcef6121314a3af897d61f58716dc507f
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-22 22:04:16 +0200

    refactor form

--- Frontend/mega_game_event_ui/src/Activities/ActivityForm.js
+++ Frontend/mega_game_event_ui/src/Activities/ActivityForm.js
@@ -1,23 +1,22 @@
 import React, { useState } from "react";
-import Calendar from 'react-calendar';
+// import Calendar from 'react-calendar';
+import { Button, Form, Segment } from 'semantic-ui-react'
 import 'react-calendar/dist/Calendar.css';
 import '../Styles/Calendar.css';
 
 function ActivityFormController () {
-  const [inputs, setInputs] = useState({});
+  const [inputs, setInputs] = useState({
+    id:"",
+    name:"",
+    startDate:"",
+    endDate:"",
+    description:"",
+    location:""
+  });
 
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs(values => ({...values, [name]: value}));
-  }
-
-  const handleStartDateChange = (value) => {
-    setInputs(values => ({...values, ["startDate"]: value}));
-  }
-
-  const handleEndDateChange = (value) => {
-    setInputs(values => ({...values, ["endDate"]: value}));
+  function handleInputChange(event){
+    const {name, value} = event.target;
+    setInputs({...inputs, [name]: value});
   }
 
   const handleSubmit = (event) => {
@@ -30,33 +29,19 @@ function ActivityFormController () {
     });
 
     event.preventDefault();
-    // alert(JSON.stringify(inputs))
   }
   
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Activity Name
-      <input
-        type="text"
-        name="name"
-        value={inputs.name || ""}
-        onChange={handleChange}
-      />
-      </label>
-      <label>Description
-        <input
-          type="text"
-          name="description"
-          value={inputs.description || ""}
-          onChange={handleChange}
-        />
-      </label>
-      <p>Start Date</p>
-      <Calendar value={inputs.startDate} onChange={handleStartDateChange}/>
-      <p>End Date</p>
-      <Calendar value={inputs.endDate} onChange={handleEndDateChange}/>
-      <button type="submit">Submit</button>
-    </form>
+    <Segment inverted>
+      <Form autocomplete="off">
+        <Form.Input placeholder="Name" value={inputs.name} name="name" onChange={handleInputChange}/>
+        <Form.TextArea placeholder='Description' value={inputs.description } name='description' onChange={handleInputChange}/>
+        <Form.Input type='date' placeholder='Start date' value={inputs.date } name='startDate' onChange={handleInputChange}/>
+        <Form.Input type='date' placeholder='End date' value={inputs.date } name='endDate' onChange={handleInputChange}/>
+        <Form.Input placeholder='Location' value={inputs.location } name='location' onChange={handleInputChange}/>
+        <Button onClick={handleSubmit} floated="right" positive type='button' content='Submit'/>
+      </Form>
+    </Segment>
   );
 }
 
