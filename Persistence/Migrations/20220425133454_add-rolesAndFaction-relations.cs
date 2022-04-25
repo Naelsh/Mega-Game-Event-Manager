c6ea08a9bce3d2f64267b681a2bd48a2ac161d39
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-25 15:36:56 +0200

    update database

---
+++ Persistence/Migrations/20220425133454_add-rolesAndFaction-relations.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class addrolesAndFactionrelations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ActivityId",
+                table: "Factions",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Factions_ActivityId",
+                table: "Factions",
+                column: "ActivityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Factions_Activities_ActivityId",
+                table: "Factions",
+                column: "ActivityId",
+                principalTable: "Activities",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Factions_Activities_ActivityId",
+                table: "Factions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Factions_ActivityId",
+                table: "Factions");
+
+            migrationBuilder.DropColumn(
+                name: "ActivityId",
+                table: "Factions");
+        }
+    }
+}
