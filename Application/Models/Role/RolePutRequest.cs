80b1b4ea9d7d9d0284e7f8ed10aecb6722fafa75
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-26 06:45:16 +0200

    add models for role

---
+++ Application/Models/Role/RolePutRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Models.Role;
+
+public class RolePutRequest
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Description { get; set; } = "";
+    public int FactionId { get; set; } = 0;
+    public int UserId { get; set; }
+}
