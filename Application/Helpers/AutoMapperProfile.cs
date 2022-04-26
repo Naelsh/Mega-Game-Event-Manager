8d5481a1405e1d76f20337b4a4e843f6051afb38
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-26 06:11:28 +0200

    add automapper for faction

--- Application/Helpers/AutoMapperProfile.cs
+++ Application/Helpers/AutoMapperProfile.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Models.Activity;
+using Application.Models.Faction;
 using AutoMapper;
 using Domain;
 
@@ -10,6 +11,19 @@ public class AutoMapperProfile : Profile
     {
         CreateMap<ActivityPostRequest, Activity>();
         CreateMap<ActivityUpdateRequest, Activity>()
+            .ForAllMembers(x => x.Condition(
+                (src, dest, prop) =>
+                {
+                    // ignore null & empty string properties
+                    if (prop == null) return false;
+                    if (prop.GetType() == typeof(string) && string.IsNullOrEmpty((string)prop)) return false;
+
+                    return true;
+                }
+            ));
+
+        CreateMap<FactionPostRequest, Faction>();
+        CreateMap<FactionUpdateRequest, Faction>()
             .ForAllMembers(x => x.Condition(
                 (src, dest, prop) =>
                 {
