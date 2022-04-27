23dca9168cf8ab77d4d92424b40b393503d9d4cd
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-27 13:02:29 +0200

    add data anotations for faction models

--- Application/Models/Faction/FactionUpdateRequest.cs
+++ Application/Models/Faction/FactionUpdateRequest.cs
@@ -1,8 +1,13 @@
-﻿namespace Application.Models.Faction;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Models.Faction;
 
 public class FactionUpdateRequest
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
+    [StringLength(50, MinimumLength = 1, ErrorMessage = "{0} length must be between {2} and {1} characters")]
+    public string Name { get; set; } = "";
+    [StringLength(10000, MinimumLength = 1, ErrorMessage = "{0} length must be between {2} and {1} characters")]
+    public string Description { get; set; } = "";
+    [Range(1, int.MaxValue, ErrorMessage = "{0} needs to be a positive integer")]
     public int ActivityId { get; set; }
 }
