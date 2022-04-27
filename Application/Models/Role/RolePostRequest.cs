46b847ff9c11131fd1dfe0e28620b6bb74636f2b
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-27 06:59:29 +0200

    remove id as a parameter for role models

--- Application/Models/Role/RolePostRequest.cs
+++ Application/Models/Role/RolePostRequest.cs
@@ -5,8 +5,6 @@ namespace Application.Models.Role;
 public class RolePostRequest
 {
     [Required]
-    public int Id { get; set; }
-    [Required]
     public string Name { get; set; } = "";
     [Required]
     public string Description { get; set; } = "";
--- Application/Models/Role/RoleUpdateRequest.cs
+++ Application/Models/Role/RoleUpdateRequest.cs
@@ -2,7 +2,6 @@
 
 public class RoleUpdateRequest
 {
-    public int Id { get; set; }
     public string Name { get; set; } = "";
     public string Description { get; set; } = "";
     public int FactionId { get; set; } = 0;
