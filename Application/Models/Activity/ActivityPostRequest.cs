268bdfd8d70a80e7b436b35f49d2156d644c7dbc
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-27 07:22:18 +0200

    add datavalidation for activity models

--- Application/Models/Activity/ActivityPostRequest.cs
+++ Application/Models/Activity/ActivityPostRequest.cs
@@ -5,11 +5,14 @@ namespace Application.Models.Activity;
 public class ActivityPostRequest
 {
     [Required]
+    [StringLength(50, ErrorMessage = "{0} length must be between {2} and {1} characters", MinimumLength = 4)]
     public string? Name { get; set; }
     [Required]
     public DateTime StartDate { get; set; }
     [Required]
     public DateTime EndDate { get; set; }
+    [StringLength(10000, ErrorMessage = "{0} length must be less than {1} characters")]
     public string? Description { get; set; }
+    [StringLength(100, ErrorMessage = "{0} length must be less than {1} characters")]
     public string? Location { get; set; }
 }
--- Application/Models/Activity/ActivityUpdateRequest.cs
+++ Application/Models/Activity/ActivityUpdateRequest.cs
@@ -1,10 +1,15 @@
-﻿namespace Application.Models.Activity;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Models.Activity;
 
 public class ActivityUpdateRequest
 {
+    [StringLength(50, ErrorMessage = "{0} length must be between {2} and {1} characters", MinimumLength = 4)]
     public string? Name { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
+    [StringLength(10000, ErrorMessage = "{0} length must be less than {1} characters")]
     public string? Description { get; set; }
+    [StringLength(100, ErrorMessage = "{0} length must be less than {1} characters")]
     public string? Location { get; set; }
 }
