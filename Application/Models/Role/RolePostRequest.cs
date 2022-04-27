c5d769cd48fd6dd9e2f63f3793822224b365c0e2
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-27 13:05:18 +0200

    add validation for role models

--- Application/Models/Role/RolePostRequest.cs
+++ Application/Models/Role/RolePostRequest.cs
@@ -5,9 +5,12 @@ namespace Application.Models.Role;
 public class RolePostRequest
 {
     [Required]
+    [StringLength(50, MinimumLength = 1, ErrorMessage = "{0} length must be between {2} and {1} characters")]
     public string Name { get; set; } = "";
     [Required]
+    [StringLength(10000, MinimumLength = 1, ErrorMessage = "{0} length must be between {2} and {1} characters")]
     public string Description { get; set; } = "";
     [Required]
+    [Range(1, int.MaxValue, ErrorMessage = "{0} needs to be a positive integer")]
     public int FactionId { get; set; } = 0;
 }
--- Application/Models/Role/RoleUpdateRequest.cs
+++ Application/Models/Role/RoleUpdateRequest.cs
@@ -1,9 +1,15 @@
-﻿namespace Application.Models.Role;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Models.Role;
 
 public class RoleUpdateRequest
 {
+    [StringLength(50, MinimumLength = 1, ErrorMessage = "{0} length must be between {2} and {1} characters")]
     public string Name { get; set; } = "";
+    [StringLength(10000, MinimumLength = 1, ErrorMessage = "{0} length must be between {2} and {1} characters")]
     public string Description { get; set; } = "";
+    [Range(1, int.MaxValue, ErrorMessage = "{0} needs to be a positive integer")]
     public int FactionId { get; set; } = 0;
+    [Range(1, int.MaxValue, ErrorMessage = "{0} needs to be a positive integer")]
     public int UserId { get; set; }
 }
