7b0853f953a56d386b2fb35dea0abf0b97751819
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-28 18:06:42 +0200

    add basic models for users

---
+++ Application/Models/User/AuthenticateRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Models.User;
+
+public class AuthenticateRequest
+{
+    [Required]
+    public string Username { get; set; } = "";
+
+    [Required]
+    public string Password { get; set; } = "";
+}
---
+++ Application/Models/User/RegisterRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Models.User;
+
+public class RegisterRequest
+{
+    [Required]
+    public string FirstName { get; set; } = "";
+
+    [Required]
+    public string LastName { get; set; } = "";
+
+    [Required]
+    public string Username { get; set; } = "";
+
+    [Required]
+    public string Password { get; set; } = "";
+}
