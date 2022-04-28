d0a8d88aedb17b14800ebaf2dda84115f99af6a5
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-28 20:01:18 +0200

    add automapping for user

--- Application/Helpers/AutoMapperProfile.cs
+++ Application/Helpers/AutoMapperProfile.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Models.Activity;
 using Application.Models.Faction;
 using Application.Models.Role;
+using Application.Models.User;
 using AutoMapper;
 using Domain;
 
@@ -38,6 +39,25 @@ public class AutoMapperProfile : Profile
 
         CreateMap<RolePostRequest, Role>();
         CreateMap<RoleUpdateRequest, Role>()
+            .ForAllMembers(x => x.Condition(
+                (src, dest, prop) =>
+                {
+                    // ignore null & empty string properties
+                    if (prop == null) return false;
+                    if (prop.GetType() == typeof(string) && string.IsNullOrEmpty((string)prop)) return false;
+
+                    return true;
+                }
+            ));
+
+        // User -> AuthenticateResponse
+        CreateMap<User, AuthenticateResponse>();
+
+        // RegisterRequest -> User
+        CreateMap<RegisterRequest, User>();
+
+        // UpdateRequest -> User
+        CreateMap<UserUpdateRequest, User>()
             .ForAllMembers(x => x.Condition(
                 (src, dest, prop) =>
                 {
