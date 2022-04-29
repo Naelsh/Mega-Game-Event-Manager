ca53a178a20ac332b03dc557af408889a88aa867
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-04-29 19:56:36 +0200

    Add route to index

--- Frontend/mega_game_event_ui/src/index.js
+++ Frontend/mega_game_event_ui/src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ActivityList from './App/Features/Activities/ActivityList';
+import { ActivityForm } from './App/Features/Activities/ActivityForm';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path='activities' element={<ActivityList />} />
+      <Route path='new-activity' element={<ActivityForm />} />
+    </Routes>
+  </BrowserRouter>
 );
