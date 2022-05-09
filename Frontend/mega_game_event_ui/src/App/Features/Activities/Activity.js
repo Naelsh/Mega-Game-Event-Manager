1cf940d46d09f7a4242ba153740158e1f06bb2ca
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-05-09 16:55:07 +0200

    add editmode to activitydetail

--- Frontend/mega_game_event_ui/src/App/Features/Activities/Activity.js
+++ Frontend/mega_game_event_ui/src/App/Features/Activities/Activity.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Activity.css';
 
 function Activity (props) {
+  const editLink = "/activitydetails/" + props.activity.id;
   return (
     <li className='activity'>
       <h3>{props.activity.name}</h3>
@@ -9,6 +11,7 @@ function Activity (props) {
       <p><strong>Ends:</strong> {props.activity.endDate}</p>
       <p><strong>Location:</strong> {props.activity.location}</p>
       <p>{props.activity.description}</p>
+      <Link to={editLink}>Edit</Link>
     </li>
   );
 }
