511f6ad3b6fbc15d0c4dac96dfe84cfa49932c85
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-05-10 12:06:17 +0200

    add basic config for identiyserver

--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -1,5 +1,19 @@
+using IdentityServer.Config;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var service = builder.Services;
+
+service.AddIdentityServer()
+    .AddInMemoryApiResources(IdConfig.apiResources)
+    .AddInMemoryApiScopes(IdConfig.apiScopes)
+    .AddInMemoryClients(IdConfig.clients)
+    .AddInMemoryIdentityResources(IdConfig.identityResources)
+    .AddTestUsers(IdConfig.testUsers)
+    .AddDeveloperSigningCredential();
+
 var app = builder.Build();
+
+app.UseIdentityServer();
 
 app.Run();
