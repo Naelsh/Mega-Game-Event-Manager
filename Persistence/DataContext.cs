89da7736f8e07448a83f41460dcf4a3003874eaf
Niklas Lindblad <niklas.o.lindblad@gmail.com>
2022-05-19 10:16:37 +0200

    add inmemory dbcontext

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,52 +1,52 @@
-﻿using Domain;
+﻿namespace Persistence;
+
+using Domain;
 using Microsoft.EntityFrameworkCore;
 
-namespace Persistence
+
+public class DataContext : DbContext
 {
-    public class DataContext : DbContext
+    public DbSet<Activity> Activities { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<Faction> Factions { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    public DataContext() { }
+
+    public DataContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        public DbSet<Activity> Activities { get; set; }
-        public DbSet<Role> Roles { get; set; }
-        public DbSet<Faction> Factions { get; set; }
-        public DbSet<User> Users { get; set; }
+        #region ConnectionStrings
+        // Laptop
+        //string connectionString =
+        //    @"Data Source=DESKTOP-EJ7V12L\SQLEXPRESS01;" +
+        //    @"Initial Catalog = MegaGameEventManager;" +
+        //    @"Integrated Security=true";
 
-        public DataContext() { }
+        //Stationär
+        string connectionString =
+            @"Data Source=DESKTOP-JC3MCVE;" +
+            @"Initial Catalog = MegaGameEventManager;" +
+            @"Integrated Security=true";
+        #endregion
 
-        public DataContext(DbContextOptions options) : base(options) { }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        if (!optionsBuilder.IsConfigured)
         {
-            #region ConnectionStrings
-            // Laptop
-            //string connectionString =
-            //    @"Data Source=DESKTOP-EJ7V12L\SQLEXPRESS01;" +
-            //    @"Initial Catalog = MegaGameEventManager;" +
-            //    @"Integrated Security=true";
-
-            //Stationär
-            string connectionString =
-                @"Data Source=DESKTOP-JC3MCVE;" +
-                @"Initial Catalog = MegaGameEventManager;" +
-                @"Integrated Security=true";
-            #endregion
-
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer(connectionString);
-            }
-            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(connectionString);
         }
+        base.OnConfiguring(optionsBuilder);
+    }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            SetupActivity(modelBuilder);
-            base.OnModelCreating(modelBuilder);
-        }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        SetupActivity(modelBuilder);
+        base.OnModelCreating(modelBuilder);
+    }
 
-        private static void SetupActivity(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Activity>().Property(a => a.StartDate).IsRequired();
-            modelBuilder.Entity<Activity>().Property(a => a.EndDate).IsRequired();
-        }
+    private static void SetupActivity(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Activity>().Property(a => a.StartDate).IsRequired();
+        modelBuilder.Entity<Activity>().Property(a => a.EndDate).IsRequired();
     }
 }
